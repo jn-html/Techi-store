0126fe260cf172601d8ee538a7c8f4af7f6202b5
jn-html <marmainjean@gmail.com>
2020-06-13 13:35:10 +0200

    Style-component intro

--- src/App.js
+++ src/App.js
@@ -3,15 +3,25 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import {FaHome} from 'react-icons/fa';
+import styled from 'styled-components';
 
 class App extends Component {
   render() {
     return (
       <h1>
         Hello from tech store<FaHome />
+        <Button large>hello styled compo</Button>
       </h1>
     );
   }
 }
+
+const color = '#f15025'
+const Button = styled.button`
+  color: white;
+  background: ${color};
+  /* font-size: 2rem; */
+  font-size: ${(props) => props.large ? '3rem' : '1rem'};
+`;
 
 export default App;
