7da140107c3fbacbd004549f36e127c4115ff6bf
jn-html <marmainjean@gmail.com>
2020-06-13 11:11:44 +0200

    React Icons intro & github/Netlify pipeline

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import {FaHome} from 'react-icons/fa';
+
 class App extends Component {
   render() {
     return (
       <h1>
-        Hello from tech store
+        Hello from tech store<FaHome />
       </h1>
     );
   }
