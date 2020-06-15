d128f9fa1d0fcd09e402a0a3fba1327591a3d0d5
jn-html <marmainjean@gmail.com>
2020-06-15 09:27:26 +0200

    React Router Setup

--- src/App.js
+++ src/App.js
@@ -9,14 +9,27 @@ import About from './pages/AboutPage';
 import Products from './pages/ProductsPage';
 import Contact from './pages/ContactPage';
 import SingleProduct from './pages/SingleProductPage';
+import Cart from './pages/CartPage';
 import Default from './pages/Default';
+
+import {Route, Switch} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <h1>
-        Hello from tech store<FaHome />
-      </h1>
+      <>
+        {/* navbar, sidebar, cart */}
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about"  component={About} />
+          <Route path="/contact"  component={Contact} />
+          <Route path="/products" exact component={Products} />
+          <Route path="/products/:id"  component={SingleProduct} />
+          <Route path="/cart"  component={Cart} />
+          <Route component={Default} />
+
+        </Switch>
+      </>
     ); 
   }
 }
