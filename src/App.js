deafd35ed904b01c71a080d1650cae355036b258
jn-html <marmainjean@gmail.com>
2020-06-15 10:39:43 +0200

    Setup Navbar-Sidebar-Sidecart-Footer component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-import {FaHome} from 'react-icons/fa';
-
+import {Route, Switch} from 'react-router-dom';
+// import {FaHome} from 'react-icons/fa';
 import Home from './pages/HomePage';
 import About from './pages/AboutPage';
 import Products from './pages/ProductsPage';
@@ -11,14 +10,20 @@ import Contact from './pages/ContactPage';
 import SingleProduct from './pages/SingleProductPage';
 import Cart from './pages/CartPage';
 import Default from './pages/Default';
+import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+import SideCart from './components/SideCart';
+import Footer from './components/Footer';
 
-import {Route, Switch} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <>
         {/* navbar, sidebar, cart */}
+        <Navbar />
+        <Sidebar />
+        <SideCart />
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/about"  component={About} />
@@ -27,8 +32,8 @@ class App extends Component {
           <Route path="/products/:id"  component={SingleProduct} />
           <Route path="/cart"  component={Cart} />
           <Route component={Default} />
-
         </Switch>
+        <Footer/>
       </>
     ); 
   }
