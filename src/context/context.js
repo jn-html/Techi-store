2c3bf06a98be092b814da2e304ca322843bf16a3
jn-html <marmainjean@gmail.com>
2020-06-16 11:26:45 +0200

    Sidebar Component w/ toggle sidebar

--- src/context/context.js
+++ src/context/context.js
@@ -1,46 +1,46 @@
-import React, { Component } from 'react';
-
-
+import React, { Component } from "react";
+import { linkData } from "./linkData";
 const ProductContext = React.createContext();
-
-class ProductProvider extends Component{
-   state = {
-      sidebarOpen: false,
-      cartOpen: false,
-      cartItems: 0
-   }
-
-   handleCart= () => {
-      this.setState({CartOpen: !this.state.sidebarOpen})
-   }
-
-   handleCart= () => {
-      this.setState({sideOpen: !this.state.sidebarOpen})
-   }
-   // close cart
-   closeCart= () => {
-      this.setState({cartOpen: false})
-   }
-   // open
-   openCart= () => {
-      this.setState({cartOpen: true})
-   }
-
-   render(){
-      return (
-         <ProductContext.Provider value={{
-            // sidebarOpen: this.state.sidebarOpen,
-            ...this.state,
-            handleSidebar: this.handleSidebar,
-            handleCart: this.handleCart,
-            closeCart: this.closeCart,
-            openCart: this.openCart
-            }}
-         >
-         {this.props.children}
-         </ProductContext.Provider>
-      )   
-   }
+//Provider
+//Consumer
+class ProductProvider extends Component {
+  state = {
+    sidebarOpen: false,
+    cartOpen: false,
+    cartItems: 110,
+    links: linkData
+  };
+  // handle sidebar
+  handleSidebar = () => {
+    this.setState({ sidebarOpen: !this.state.sidebarOpen });
+  };
+  // hanldle sart
+  handleCart = () => {
+    this.setState({ cartOpen: !this.state.sidebarOpen });
+  };
+  //close cart
+  closeCart = () => {
+    this.setState({ cartOpen: false });
+  };
+  // open
+  openCart = () => {
+    this.setState({ cartOpen: true });
+  };
+  render() {
+    return (
+      <ProductContext.Provider
+        value={{
+          ...this.state,
+          handleSidebar: this.handleSidebar,
+          handleCart: this.handleCart,
+          closeCart: this.closeCart,
+          openCart: this.openCart
+        }}
+      >
+        {this.props.children}
+      </ProductContext.Provider>
+    );
+  }
 }
 
 const ProductConsumer = ProductContext.Consumer;
