1c0b89a8c6cabb0cbc1a5a033cea99b824400aaa
jn-html <marmainjean@gmail.com>
2020-06-17 17:56:17 +0200

    Main Link Class + styling in App.css and in Homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -7,7 +7,7 @@ export default function HomePage() {
    return (
       <>
          <Hero title="awesome Gadgets" max="true">
-            <Link to='/products'>our products</Link>
+            <Link to='/products' className="main-link" style={{margin:'2rem'}}>our products</Link>
          </Hero>
       </ >
    );
