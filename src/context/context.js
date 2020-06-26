0c538f6b5c87e6573e240a0135fab9a23bd3008d
jn-html <marmainjean@gmail.com>
2020-06-26 14:43:46 +0200

    Filter Products Context Setup

--- src/context/context.js
+++ src/context/context.js
@@ -29,7 +29,13 @@ class ProductProvider extends Component {
     filteredProducts: [],
     featuredProducts: [],
     singleProducts: {},
-    loading: true
+    loading: true,
+    search: '',
+    price: 0,
+    min: 0,
+    max: 0,
+    company: "all",
+    shipping: false
   };
 
   componentDidMount() {
@@ -54,6 +60,10 @@ class ProductProvider extends Component {
     });
     // featured products
     let featuredProducts = storeProducts.filter(item => item.featured === true);
+
+    // get max price
+    let maxPrice = Math.max(...storeProducts.map(item => item.price));
+
     this.setState({
         // ES5 
         // storeProducts : storeProducts
@@ -63,7 +73,9 @@ class ProductProvider extends Component {
         featuredProducts,
         cart: this.getStorageCart(),
         singleProduct: this.getStorageProduct(),
-        loading: false
+        loading: false,
+        price: maxPrice,
+        max: maxPrice
       },
       () => {
         this.addTotals();
@@ -283,6 +295,17 @@ class ProductProvider extends Component {
     })
   };
 
+  // handle filtering
+  // look for event to check (typing, box selectBox etc...)
+  handleChange = (event) => {
+    console.log(event)
+  }
+
+  // sort data
+  // 
+  sortData = () => {
+
+  }
 
   render() {
     return ( <
@@ -298,7 +321,8 @@ class ProductProvider extends Component {
           increment: this.increment,
           decrement: this.decrement,
           removeItem: this.removeItem,
-          clearCart: this.clearCart
+          clearCart: this.clearCart,
+          handleChange: this.handleChange
         }
       } >
       {
